690c4e61964908e9d483ed75df2e20bb302296cb
Estenio Rocha <angelojuliorafel@gmail.com>
2021-11-29 13:14:00 -0300

    requisito-1-completo

---
+++ fundamentos/bloco-04-inducao-a-javascript-e-logica-de-programacao/dia-01/script.js
@@ -0,0 +1,28 @@
+/* 1x1*/
+const ax11 = 10;
+const bx11 = 100;
+let cx11 = ax11 + bx11;
+console.log(cx11)
+/* 1x2 */
+const ax12 = 10;
+const bx12 = 100;
+let cx12 = ax12 - bx12;
+console.log(cx12)
+/* 1x3 */
+const ax13 = 10;
+const bx13 = 100;
+let cx13 = ax13 * bx13;
+console.log(cx13)
+/* 1x4 */
+const ax14 = 10;
+const bx14 = 100;
+let cx14 = ax14 / bx14;
+console.log(cx14)
+/* 1x5 */
+const ax15 = 100;
+const bx15 = 100;
+let cx15 = ax15 % bx15;
+console.log(cx15)
+/*  */
+/*  */
+/*  */
