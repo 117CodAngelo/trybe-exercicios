bc59f9e6eceaa835798928be8a65439021625953
estenio <angelojuliorafel@gmail.com>
2021-12-01 13:36:10 -0300

    requisito 3 completo

--- fundamentos/bloco-04-inducao-a-javascript-e-logica-de-programacao/dia-02/script.js
+++ fundamentos/bloco-04-inducao-a-javascript-e-logica-de-programacao/dia-02/script.js
@@ -3,11 +3,21 @@
 for (let n of numbers) {
     console.log(n);
 } */
+
 /* 2x1 */
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
 for (let n of numbers) {
     soma = soma + n;
 }
-console.log(soma);
+console.log(soma); */
+
+/* 3x1 */
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let n of numbers) {
+    soma = soma + n;
+}
+let mediaAritimetica = soma / numbers.length
+console.log(mediaAritimetica);
