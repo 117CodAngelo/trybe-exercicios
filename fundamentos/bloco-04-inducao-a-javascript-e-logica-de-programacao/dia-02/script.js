08f8e64a9f582c9d128cedc6964b88b5fd32f165
estenio <angelojuliorafel@gmail.com>
2021-12-01 13:59:07 -0300

    requisito 5 completo

--- fundamentos/bloco-04-inducao-a-javascript-e-logica-de-programacao/dia-02/script.js
+++ fundamentos/bloco-04-inducao-a-javascript-e-logica-de-programacao/dia-02/script.js
@@ -23,7 +23,7 @@ let mediaAritimetica = soma / numbers.length
 console.log(mediaAritimetica); */
 
 /* 4x1 */
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for (let n of numbers) {
     soma = soma + n;
@@ -33,4 +33,16 @@ if (mediaAritimetica > 20) {
     console.log("valor maior que 20")
 } else {
     console.log("valor menor ou igual a 20")
+} */
+
+/* 5x1 */
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = null;
+for (let n of numbers) {
+    if (maiorNumero <= n) {
+        maiorNumero = n
+    } else {
+        console.log("e menor")
+    }
 }
+console.log(maiorNumero)
